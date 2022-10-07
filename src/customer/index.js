26bbdbdc2ffa1be54a0e26c50ff516661252ed86
Keelen Fisher <keelencarrera@gmail.com>
2022-10-06 19:10:54 -0500

    proof of life, shows event

---
+++ src/customer/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { io } = require('socket.io-client');
+const socket = io('http://localhost:3002');
+
+const customerPickUp = require('./customerPickUp');
+const customerPickUp2 = customerPickUp(socket);
+
+const Chance = require('chance');
+let chance = new Chance();
+
+setTimeout(() => {
+  console.log('---------------------Client Has Started Their Order, Ready for Pickup-------------------');
+  const payload = {
+    store: `Kenny G's Saxophone 'n More Store`,
+    orderID: chance.guid(),
+    customer: chance.name(),
+    address: chance.address(),
+  };
+  customerPickUp2(payload);
+}, 5000);
--- src/hub/index.js
+++ src/hub/index.js
@@ -10,6 +10,25 @@ const iHub = new Server(PORT);
 
 iHub.on('connection', (socket) => {
   console.log('This id connected to the main server', socket.id);
+  
+  socket.on('CUSTOMER', (payload) => {
+    console.log('This is the order from the customer: ', payload);
+    socket.emit('NEWCUSTOMER', payload);
+    // enter the queue 
+  }); 
+
+  socket.on('IN-TRANSIT', (payload) => {
+    console.log('This is the Vendor side, received the order and is in process.', payload);
+    // enter the queue
+  }); 
+
+  socket.on('DELIVERED', (payload) => {
+    console.log('This is the Vendor side, order confirmed and is ready to be delivered.', payload);
+    socket.emit('ORDERCONFIRMED');
+    // enter the queue
+  }); 
 
 });
+
+
 
