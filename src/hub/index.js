b20c247db06d876fe9d238ccee3b9bbe603abfbb
Keelen Fisher <keelencarrera@gmail.com>
2022-10-08 12:46:09 -0500

    queue proof of life

--- src/hub/index.js
+++ src/hub/index.js
@@ -2,7 +2,7 @@
 
 const { Server } = require('socket.io');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3004;
 
 const iHub = new Server(PORT);
 
@@ -13,17 +13,37 @@ const Queue = require('../lib/queue');
 const messageQueue = new Queue();
 
 iHub.on('connection', (socket) => {
-  console.log('This id connected to the main server', socket.id);
+  console.log('This is connected to the main server', socket.id);
 
   socket.on('CUSTOMER', (payload) => {
-    console.log('This is the order from the customer: ', payload);
-    socket.broadcast.emit('NEWCUSTOMER', payload);
     // enter the queue 
+    let currentQueue = messageQueue.read(payload.queueId);
+    if (!currentQueue) {
+      let queueKey = messageQueue.store(payload.queueId, new Queue());
+      currentQueue = messageQueue.read(queueKey);
+    }
+    currentQueue.store(payload.messageId, payload);
+    console.log('This is the order from the customer: ', currentQueue.read(payload.messageId));
+    socket.broadcast.emit('NEWCUSTOMER', payload);
   });
 
   socket.on('IN-TRANSIT', (payload) => {
     console.log('This is the Vendor side, received the order and is in process.');
     // enter the queue
+    socket.emit('CONFIRMDELIVERY', payload);
+    let currentQueue = messageQueue.read(payload.queueId);
+    if (!currentQueue) {
+      let queueKey = messageQueue.store(payload.queueId, new Queue());
+      currentQueue = messageQueue.read(queueKey);
+    }
+    currentQueue.store(payload.messageId, payload);
+    console.log('This order is in transit: ', currentQueue.read(payload.messageId));
+    
+    if(!currentQueue){
+      throw new Error('no queue created');
+    }
+    let message = currentQueue.remove(payload.messageId);
+    console.log('This has been deleted from the key: TRANSIT', message);
   });
 
   // delete from the queue/ 2  
@@ -31,12 +51,23 @@ iHub.on('connection', (socket) => {
     console.log('Order confirmed and is ready to be delivered.');
     socket.broadcast.emit('ORDERCONFIRMED');
     // enter the queue
+    let currentQueue = messageQueue.read('customers');
+    console.log(messageQueue);
+    if(!currentQueue){
+      throw new Error('no queue created');
+    }
+    let message = currentQueue.remove(payload.messageId);
+    console.log('This has been deleted from the key: DELIVERED', message);
   });
 
-
-
-  //   socket.on('RECEIVED', (payload) => {
-  //     let currentQueue = messageQueue.read(payload.queueId);
-  //   });
+  socket.on('GET_INSTRUMENT', (payload) => {
+    console.log('This happened');
+    let currentQueue = messageQueue.read(payload.queueId);
+    if(currentQueue && currentQueue.data){
+      Object.keys(currentQueue.data).forEach(messageId => {
+        socket.emit('CUSTOMER', currentQueue.read(messageId));
+      });
+    }
+  });
 
 });
--- src/vendor/vendorPickUp.js
+++ src/vendor/vendorPickUp.js
@@ -1,15 +1,8 @@
-'use strict';
+// 'use strict';
 
-module.exports = (socket) => (payload) => {
-
-  setTimeout(() => {
-    console.log(`CONFIRM the order for: ${payload.customer}`);
-    socket.emit('IN-TRANSIT', payload)
-  }, 4000); 
-
-  setTimeout(() => {
-    console.log(`DELIVER the order for: ${payload.customer}`);
-    socket.emit('DELIVERED', payload);
-  }, 8000)
-
-};
+// module.exports = (socket) => (payload) => {
+//   setTimeout(() => {
+//     console.log(`CONFIRM the order for: ${payload.customer}`);
+//     socket.emit('IN-TRANSIT', payload);
+//   }, 4000); 
+// };
